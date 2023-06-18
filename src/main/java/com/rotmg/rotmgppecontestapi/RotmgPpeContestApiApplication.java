4955a4d9d87c101b84018d8db434c2f925959cdf
lani-lee <55515583+lani-lee@users.noreply.github.com>
2023-06-17 19:31:50 -0500

    Initial commit

---
+++ src/main/java/com/rotmg/rotmgppecontestapi/RotmgPpeContestApiApplication.java
@@ -0,0 +1,13 @@
+package com.rotmg.rotmgppecontestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotmgPpeContestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RotmgPpeContestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rotmg/rotmgppecontestapi/RotmgPpeContestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rotmg.rotmgppecontestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RotmgPpeContestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
